effb276ac15505f3e4dde77ea5aceedb12b747f9
y-kurami <yosuke.kurami@gmail.com>
2017-05-26 19:20:06 +0900

    sample

---
+++ graphql_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+data=$(curl -H "Authorization: token $GITHUB_TOKEN" -s -d @- https://api.github.com/graphql << GQL
+{ "query": "
+  query {
+    viewer {
+      login
+    }
+  }
+" }
+GQL)
+
+echo $data | jq -r .data.viewer.login
