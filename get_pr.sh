21ed8fc2defe4677970615cdab9cfb4a8bd6e75b
y-kurami <yosuke.kurami@gmail.com>
2017-06-05 16:30:01 +0900

    add get pr sh

---
+++ get_pr.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function getPR() {
+  BRANCH_NAME=$(git branch | grep -E "^\*" | cut -f 2 -d " ")
+  echo "branch: $BRANCH_NAME" >&2
+  data=$(curl -H "Authorization: token $GITHUB_TOKEN" -s -d @- https://api.github.com/graphql << GQL
+{ "query": "
+  query {
+    repository(owner:\"Quramy\", name: \"graphql-script-sample\") {
+      ref(qualifiedName: \"$BRANCH_NAME\") {
+        name,
+        associatedPullRequests(first: 1) {
+          totalCount,
+          edges {
+            node {
+              id
+              comments(first: 100) {
+                edges {
+                  node {
+                    bodyText
+                  }
+                } 
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+" }
+GQL
+  )
+  echo $data
+}
+
+getPR
